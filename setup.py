044debd21f881e93a069d9a646afaf033b68e85d
SubhankarGhosh1233 <subhankar333ghosh@gmail.com>
2023-02-26 16:02:20 +0530

    Updated setup.py and requirments.txt

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages,setup
+
+def get_requirements():
+    pass
+
+setup(
+    name = "sensor",
+    version = "0.0.1",
+    author = "subhankar",
+    author_email = "subhankarghoshds@gmail.com",
+    packages = find_packages(),
+    install_requires = get_requirements(),
+
+)
